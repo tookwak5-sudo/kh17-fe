db8ce8d4870e278ec5d3ee91b9e3b92d393d93b0
ansgmlrjs <moonheegun1208@gmail.com>
2026-06-30 16:16:52 +0900

    demo02 react 기본설정

--- reactjs/demo02/src/App.jsx
+++ reactjs/demo02/src/App.jsx
@@ -5,10 +5,12 @@ import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  //절대규칙 : 화면은 무조건 하나로 묶어서 반환
   return (
-    <h1>KH정보교육원</h1>
+    <>
+      <h1>KH정보교육원</h1>
+      <p>저희 학원에 오신 것을 환영합니다</p>
+    </>
   )
 }
 
