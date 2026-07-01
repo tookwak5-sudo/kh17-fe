9a67a2e2175ed674e8af4a388ab302b849a2fe1e
ansgmlrjs <moonheegun1208@gmail.com>
2026-07-01 10:07:47 +0900

    입력창 실습

--- reactjs/demo04/src/App.jsx
+++ reactjs/demo04/src/App.jsx
@@ -2,16 +2,30 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  //이 화면은 이미지의 "크기"를 조절하는 것이 목표
+  //"크기"를 state로 관리해서 화면과 연결시켜두고 변경하도록 처리
   const [size, setSize] = useState(300);
+
   return (
     <>
       <h1>이미지 크기조절</h1>
-      <hr></hr>
-      <button onClick={()=>setSize(Math.max(100,size-50))}>작게</button>
-      <span>현재 : {size}px</span>
-      <button onClick={()=>setSize(Math.min(600,size+50))}>크게</button>
-      <hr></hr>
-      <img src="https://picsum.photos/500" width={size}></img>
+      <hr/>
+      <button onClick={()=>setSize(150)}>작게</button>
+      <button onClick={()=>setSize(300)}>보통</button>
+      <button onClick={()=>setSize(400)}>크게</button>
+      
+      <div>
+        현재 크기 : {size}px
+        &nbsp;&nbsp;
+        <button onClick={()=>setSize(Math.min(400, size+10))}>+</button>
+        <button onClick={()=>setSize(Math.max(150, size-10))}>-</button>
+      </div>
+
+      <hr/>
+
+      <img src="https://picsum.photos/300"
+          className="target"
+          width={size} height={size}/>
     </>
   )
 }
--- reactjs/demo05/src/App.jsx
+++ reactjs/demo05/src/App.jsx
@@ -1,11 +1,30 @@
+import { useState } from 'react';
 import './App.css'
 
 function App() {
-
+  const [value, setValue] = useState(0);
   return (
     <>
-      <h1>이체 금액 입력</h1>
-      <input/>
+      <h1>이체 금액 입력화면</h1>
+      {/* <div>{value}원</div> */}
+      <input value={value} readOnly/>
+
+      <hr/>
+
+      <button onClick={()=>setValue(value + 10000000)}>천만</button>
+      <button onClick={()=>setValue(value + 1000000)}>백만</button>
+      <button onClick={()=>setValue(value + 100000)}>십만</button>
+      <button onClick={()=>setValue(value + 10000)}>만</button>
+      <button onClick={()=>setValue(value + 1000)}>천</button>
+      <button onClick={()=>setValue(value + 100)}>백</button>
+      <button onClick={()=>setValue(value + 10)}>십</button>
+      <button onClick={()=>setValue(value + 1)}>일</button>
+      <hr/>
+      <button onClick={()=>setValue(parseInt(value / 10))}>x</button>
+      <button onClick={()=>setValue(0)}>지우기</button>
+      <div>
+        금액을 입력하세요
+      </div>
     </>
   )
 }
