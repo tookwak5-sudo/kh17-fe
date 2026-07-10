80e3f094a576fdb1d58594e98a494a3d56e063a4
tookwak5-sudo <tookwak5@gmail.com>
2026-07-10 17:22:10 +0900

    자동완성(lodash - throtle, debounce 사용)

--- reactjs/home/src/components/country/CountrySearch.jsx
+++ reactjs/home/src/components/country/CountrySearch.jsx
@@ -2,42 +2,37 @@ import Jumbotron from "@templates/Jumbotron";
 import axios from "axios";
 import { useCallback, useEffect, useState } from "react";
 import { Col, Form, ListGroup, Row } from "react-bootstrap";
+import { throttle, debounce } from "lodash-es";
 
 export default function CountrySearch() {
     //state
     const [keyword, setKeyword] = useState("");
     const [searchList, setSearchList] = useState([]);
-    const [composition, setComposition] = useState(false); //입력 글자가 조합중 여부 상태값
 
-    //입력중인 값을 제거한 검색용 키워드
-    const [result, setResult] = useState("");
-
+    //callback
     const changeKeyword = useCallback(e=>{
-        //e.data가 존재하면 onCompositionUdate 상황, 없으면 onChange상황
-        //console.log(e.data);
-        if(e.data !== undefined) {//onCompositionUate 상황이면 e.data를 확인해서 keyword를 갱신
-            // console.log(e.target.value, e.data);
-            setResult(e.target.value.substring(0, e.target.value.length - 1));
-        }
-        else{//onChange상황 (기존처럼 keyword를 업데이트)
             setKeyword(e.target.value);
-        }
     }, []);
 
     useEffect(()=>{
-        searchKeyword();
-    }, [result]);
-
-    const searchKeyword = useCallback(async ()=>{
-        if(result.length === 0) {
+        searchKeyword(keyword);
+    }, [keyword]);
+    //throttle, debounce 설정 시 주의사항
+    //throttle(함수, 실행주기) → 새로운 함수가 생성됨 -> 입력 중에 발생
+    //debounce(함수, 실행주기) → 새로운 함수가 생성됨 -> blur와 비슷하게 쓰임 입력(event)을 쉬면 발생
+    //-(주의) 함수를 만들 때 연관항목을 설정하지 말아야 한다 (함수가 재생성이 ㄷ안되야 함)
+    //-일반적으로 실행주기는 250ms ~ 350ms 정도가 적당 (1초에 3~4번)
+    const searchKeyword = useCallback(throttle(async (keyword)=>{
+        if(keyword.length === 0) {
             //키워드가 업으면 검색결과 지워
             setSearchList([]);
             return;
         }
+        console.log("searchKeyword 실행");
 
         const response = await axios.get(`/api/country/countryName/${keyword}`);
         setSearchList(response.data);
-    }, [result]);
+    }, 350), []);
 
     return(<>
         <Jumbotron title="국가명 검색 샘플"/> 
@@ -48,13 +43,7 @@ export default function CountrySearch() {
                 <div className="position-relative">
                 <Form.Control placeholder="검색어 입력" size="lg"
                     value={keyword} 
-                    onChange={changeKeyword}
-                    onCompositionStart={e=>setComposition(true)}
-                    onCompositionUpdate={changeKeyword}
-                    onCompositionEnd={e=>{
-                        setResult(e.target.value);
-                        setComposition(false);
-                    }}/>
+                    onChange={changeKeyword}/>
                 <ListGroup className="position-absolute start-0 end-0 top-100">
                     {searchList.map(country=>(
                         <ListGroup.Item key={country.countryNo}>
