4b084a84f91dd2ba61d991abd8cfa1b0a19b512d
tookwak5-sudo <tookwak5@gmail.com>
2026-07-24 09:35:10 +0900

    달력 Locale ko, 날짜 형식 YYYY-MM-DD

--- reactjs/home/src/components/account/AccountChange.jsx
+++ reactjs/home/src/components/account/AccountChange.jsx
@@ -6,9 +6,17 @@ import { useKakaoPostcodePopup } from "react-daum-postcode";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 import { apiClient, certClient } from "../../utils/reaxios";
+
+import { ko } from "date-fns/locale";
 import DatePicker from "react-datepicker";
+import { registerLocale } from "react-datepicker";
+
 import "react-datepicker/dist/react-datepicker.css";
 import Swal from "sweetalert2";
+
+import dayjs from "dayjs";
+import "dayjs/locale/ko";
+dayjs.locale("ko"); //한국어로 설정
 
 export default function AccountChange() {
     //kakao post
@@ -500,13 +508,23 @@ export default function AccountChange() {
                         selected={account.accountBirth}
                         onChange={(date)=>{
                             //date가 우리가 원하는 형식이 아님
+                            //→ dayjs를 이용해서 "YYYY-MM-DD" 형태로 변경
+                            const convertDate = dayjs(date).format("YYYY-MM-DD");
+                            console.log("convertDate", convertDate);
                             setAccount(prev=>({...prev, accountBirth : date}))
                         }}
                         onBlur={checkAccountBirth}
-                        dataFormat={"yyyy-MM-dd"}
+                        dateFormat={"yyyy-MM-dd"}
                         customInput={<Form.Control/>}
                         wrapperClassName={`w-100`}
-                        className={`result.accountBirth`}/>
+                        className={`result.accountBirth`}
+                        
+                        showYearDropdown
+                        showMonthDropdown
+                        dropdownMode="select"
+
+                        locale={ko}
+                        />
                 {/* <Form.Control type="date" name="accountBirth"
                     value={account.accountBirth}
                     onChange={changeStringValue}
