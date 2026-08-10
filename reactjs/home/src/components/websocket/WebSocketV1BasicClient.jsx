8983afaf0926b97aa2fd2d317fba7ea74daded91
tookwak5-sudo <tookwak5@gmail.com>
2026-08-10 14:41:13 +0900

    외부화면 채팅 구현

--- reactjs/home/src/components/websocket/WebSocketV1BasicClient.jsx
+++ reactjs/home/src/components/websocket/WebSocketV1BasicClient.jsx
@@ -1,5 +1,5 @@
 import Jumbotron from "@templates/Jumbotron";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import SockJS from "sockjs-client";
 import { Client } from "@stomp/stompjs";
 import { Button, Col, Form, Row } from "react-bootstrap";
@@ -9,6 +9,7 @@ export default function WebSocketV1BasicClient() {
     //WebSocket은 연결을 기반으로 하기 때문에 연결에 사용할 객체가 있어야 한다
     const [client, setClient] = useState(null); //서버와의 연결정보를 가진 객체(들어오자 마자)
     const [input, setInput] = useState("");//사용자의 입력
+    const [history, setHistory] = useState([]); //수신된 메세지 이력
 
     //WebSocket 연결은 들어오자마자 해야하며, 나갈 때 반드시 해제해야 한다
     //→ 연관항목이 없는 useEffect를 사용하고 Clean-Up 함수를 생성해야 한다
@@ -40,7 +41,9 @@ export default function WebSocketV1BasicClient() {
             onConnect: () => { //연결되었을 때
                 //client.subscribe(채널명, 콜백함수);
                 client.subscribe("/public/basic", (message) => {
-                    console.log(message);
+                    // console.log(message);
+                    const json = JSON.parse(message.body);
+                    setHistory(prev=>[...prev, json]); //history에 메세지 누적시키기
                 });
             },
             //디버깅 설정(옵션)
@@ -60,7 +63,10 @@ export default function WebSocketV1BasicClient() {
 
     //메세지 전송 함수
     const sendMessage = useCallback(()=>{
-        
+        //보낼 수 있는 상태인지를 검증
+        if(isConnect === false) return;
+        if(input.trim() === "") return;
+
         //메세지 전송을 위한 JSON 데이터 생성
         const json = { content : input };
 
@@ -72,7 +78,15 @@ export default function WebSocketV1BasicClient() {
 
         //전송
         client.publish(stompMessage);
+        setInput(""); //입력값 청소
     }, [client, input]);
+
+    //client가 연결중인지 확인하는 메모
+    const isConnect = useMemo(()=>{
+        if(client === null) return false; //client가 없는 경우
+        if(client.active === false) return false; //deactivate() 상태인 경우
+        return true;
+    }, [client]);
 
     return (<>
         <Jumbotron title="WebSocket Version 1" content="기본 웹소켓 예제" />
@@ -81,14 +95,35 @@ export default function WebSocketV1BasicClient() {
             <Form.Label column sm={3}>메세지 입력</Form.Label>
             <Col sm={9}>
                 <div className="d-flex">
-                    <Form.Control type="text" 
-                            value={input} onChange={e=>setInput(e.target.value)}/>
+                    {/* 입력창과 버튼은 연결이 활성화 되어있을 경우에만 사용 가능하도록 서렂ㅇ */}
+                    <Form.Control type="text" disabled={isConnect === false}
+                            value={input} onChange={e=>setInput(e.target.value)}
+                            onKeyUp={e=>{
+                                //엔터를 누르고 올렸을 때, 전송버튼과 동일한 기능을 실행
+                                if(e.key === "Enter") sendMessage();
+                            }}
+                    />
 
                     <Button variant="success" className="text-nowrap ms-2" 
-                                                            onClick={sendMessage}>
+                            disabled={isConnect === false} onClick={sendMessage}>
                         <FaPaperPlane />
                         <span className="ms-2 d-none d-sm-inline">전송</span>
                     </Button>
+                </div>
+            </Col>
+        </Row>
+        {/* 메세지를 출력 */}
+        
+        <Row className="mt-5">
+            <Col>
+
+                {/* 메세지 영역 생성 */}
+                <div className="d-flex flex-column">
+                    {history.map((message, index)=>(
+                    <div key={index}>
+                        {message.content}
+                    </div>
+                    ))}
                 </div>
             </Col>
         </Row>
