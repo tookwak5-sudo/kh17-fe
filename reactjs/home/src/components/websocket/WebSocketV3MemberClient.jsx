a4d5283a0d97c53b2523f31089247a833752cdf9
tookwak5-sudo <tookwak5@gmail.com>
2026-08-12 11:26:06 +0900

    채팅방 생성 구현중

--- reactjs/home/src/components/websocket/WebSocketV3MemberClient.jsx
+++ reactjs/home/src/components/websocket/WebSocketV3MemberClient.jsx
@@ -18,6 +18,7 @@ export default function WebSocketV3MemberClient() {
     const loginUser = useAtomValue(loginUserState);
     const [history, setHistory] = useState([]); //메세지 이력
     const [input, setInput] = useState("");//사용자의 입력
+    const inputRef = useRef(); //입력창 제어용 리모컨
     const [users, setUsers] = useState([]); //접속한 사용자의 목록
 
     useEffect(() => {
@@ -169,6 +170,7 @@ export default function WebSocketV3MemberClient() {
                             //엔터를 누르고 올렸을 때, 전송버튼과 동일한 기능을 실행
                             if (e.key === "Enter") sendMessage();
                         }}
+                        ref={inputRef}
                     />
 
                     <Button variant="success" className="text-nowrap ms-2"
@@ -295,13 +297,20 @@ export default function WebSocketV3MemberClient() {
             </Col>
 
             {/* 사용자 목록 */}
-            <Col sm={2}>
-                <ListGroup>                  
-                    {users.map((user, index) => (
-                        <ListGroupItem  key={index} className={user.accountId === loginUser.accountId ? "activ" : ""}>
+            <Col sm={3}>
+                <ListGroup>
+                    {users.map((user,index)=>(
+                        <ListGroupItem key={index}
+                            className={user.accountId === loginUser.accountId ? "active" : ""}
+                            onClick={e=>{
+                                setInput(`/w ${user.accountId}`);
+                                inputRef.current.focus();
+                            }}
+                            style={{"cursor" : "pointer"}}>
+                            
                             <span>{user.accountId}</span>
-                            {(user.accountId === loginUser.accountId) && (
-                                <span className="ms-1 fw-bold">"(나)"</span>
+                            {user.accountId === loginUser.accountId && (
+                                <span className="ms-1 fw-bold">(나)</span>
                             )}
                         </ListGroupItem>
                     ))}
